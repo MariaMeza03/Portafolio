076ab1b251f54d413eff8d7c6bbaff2b7108d2c2
Maria Meza <mmeza@licorne.mx>
2023-08-26 03:14:36 -0600

    ...

---
+++ js/carrusel.js
@@ -0,0 +1,12 @@
+const swiper= new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    loop:"true",
+    
+    navigation: {
+    nextEl: '.services--next',
+    prevEl: '.services--prev',
+    },
+
+
+})  
